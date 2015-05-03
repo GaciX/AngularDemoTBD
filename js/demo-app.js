07cc385288d304b9893c05b6b8168d0635fcd812
Daniel Gacitua <gacix@riseup.net>
2015-05-02 22:50:10 -0300

    Fijando sistema de rutas

--- js/demo-app.js
+++ js/demo-app.js
@@ -1,13 +1,90 @@
-var app = angular.module('demoapp', ['ui.router', 'demoapp.controllers', 'demoapp.services']);
+(function(){
+	var app = angular.module('demoapp', ['ngRoute', 'appServices']);
 
-app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-	$stateProvider
+    app.config(['$routeProvider', function($routeProvider) {
+        $routeProvider.
+                when('/inicio', {templateUrl: 'inicio.html',   controller: 'HomeCtrl'}).
+                when('/descripcion', {templateUrl: 'descripcion.html',   controller: 'HomeCtrl'}).
+                when('/porque', {templateUrl: 'porque.html',   controller: 'HomeCtrl'}).
+                when('/arquitectura', {templateUrl: 'arquitectura.html',   controller: 'HomeCtrl'}).
+                //when('/inicio', {templateUrl: 'index.html',   controller: 'HomeCtrl'}).
+                //when('/inicio', {templateUrl: 'index.html',   controller: 'HomeCtrl'}).
+                when('/list', {templateUrl: 'list.html',   controller: 'ListCtrl'}).
+                when('/detail/:itemId', {templateUrl: 'detail.html',   controller: 'DetailCtrl'}).
+                when('/settings', {templateUrl: 'settings.html',   controller: 'SettingsCtrl'}).
+                otherwise({redirectTo: '/inicio'});
+	}]);
 
-	.state('inicio', {
-        url:'/',
-        templateUrl: 'inicio.html'
-        //controller: 'ControladorPrincipal'
-    })
+	app.controller('MainCtrl', function($scope, Page) {
+	    console.log(Page);
+	    $scope.page= Page; 
+	});
 
-	$urlRouterProvider.otherwise('/');
-}]);
+	app.controller('HomeCtrl', function($scope, Page) {
+	    Page.setTitle("Welcome");
+	});
+
+	var app = angular.module('appServices', []);
+
+	app.factory('Page', function($rootScope){
+	            var pageTitle = "Untitled";
+	            return {
+	                title:function(){
+	                    return pageTitle;
+	                },
+	                setTitle:function(newTitle){
+	                    pageTitle = newTitle;
+	                }
+	            }
+	        });
+
+
+	/*
+	var app = angular.module('demoapp', [], function($routeProvider, $locationProvider) {
+	  $routeProvider.when('/inicio', {
+	    templateUrl: '/index.html',
+	    controller: HomeController
+	  });
+	  $routeProvider.otherwise( { redirectTo: '/inicio'} );
+
+	  // configure html5 to get links working
+	  // If you don't do this, you URLs will be base.com/#/home rather than base.com/home
+	  $locationProvider.html5Mode(true);
+	});*/
+
+	/*app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+		$stateProvider
+
+		.state('inicio', {
+	        url:'/',
+	        templateUrl: 'inicio.html'
+	        //controller: 'ControladorPrincipal'
+	    })
+
+		$urlRouterProvider.otherwise('/');
+	}]);*/
+
+	/*var cam = "a";
+    var test = "Hola Mundo!"
+
+	app.controller('ControladorJSON', ['$scope', '$http', function($scope, $http) {
+		//$scope.camaras = cam;
+		this.texto = cam;
+		var cams = this;
+
+		console.log("holiwi");
+		$http.get('json/canon_cameras.json').
+			success(function(data, status, headers, config) {
+			  // this callback will be called asynchronously
+			  // when the response is available
+			  console.log(status);
+			  cams = angular.fromJson(cams);
+			  
+			}).
+			error(function(data, status, headers, config) {
+			  // called asynchronously if an error occurs
+			  // or server returns response with an error status.
+			  console.log(status);
+			});
+	}]);*/
+})();
--- js/demo-controllers.js
+++ js/demo-controllers.js
@@ -1,10 +1,11 @@
-var app = angular.module('demoapp.controllers', []);
+angular.module('demoapp.controllers', [])
 
+/*
 app.controller('ControladorPrincipal', ['$state', function($state) {
 	angular.element(document).ready(function () {
 		$state.go('inicio');
     });
-}]);
+}]);*/
 
 
 
