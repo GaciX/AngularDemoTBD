439be3a45e344c19fd37857df04ba187404acd73
Daniel Gacitua <gacix@riseup.net>
2015-05-02 23:14:51 -0300

    Sistema de rutas completado

--- js/demo-app.js
+++ js/demo-app.js
@@ -3,12 +3,20 @@
 
     app.config(['$routeProvider', function($routeProvider) {
         $routeProvider.
+        		// Rutas de vistas
                 when('/inicio', {templateUrl: 'inicio.html',   controller: 'HomeCtrl'}).
                 when('/descripcion', {templateUrl: 'descripcion.html',   controller: 'HomeCtrl'}).
                 when('/porque', {templateUrl: 'porque.html',   controller: 'HomeCtrl'}).
                 when('/arquitectura', {templateUrl: 'arquitectura.html',   controller: 'HomeCtrl'}).
-                //when('/inicio', {templateUrl: 'index.html',   controller: 'HomeCtrl'}).
-                //when('/inicio', {templateUrl: 'index.html',   controller: 'HomeCtrl'}).
+                when('/introduccion', {templateUrl: 'introduccion.html',   controller: 'HomeCtrl'}).
+                when('/integrantes', {templateUrl: 'inicio.html',   controller: 'HomeCtrl'}).
+                when('/tabla-contenido', {templateUrl: 'tabla.html',   controller: 'HomeCtrl'}).
+                when('/ventajas', {templateUrl: 'ventaja.html',   controller: 'HomeCtrl'}).
+                when('/desventajas', {templateUrl: 'desventajas.html',   controller: 'HomeCtrl'}).
+                when('/material', {templateUrl: 'material.html',   controller: 'HomeCtrl'}).
+                when('/referencias', {templateUrl: 'referencias.html',   controller: 'HomeCtrl'}).
+                when('/demo', {templateUrl: 'demo.html',   controller: 'DemoCtrl'}).
+                // Otras rutas
                 when('/list', {templateUrl: 'list.html',   controller: 'ListCtrl'}).
                 when('/detail/:itemId', {templateUrl: 'detail.html',   controller: 'DetailCtrl'}).
                 when('/settings', {templateUrl: 'settings.html',   controller: 'SettingsCtrl'}).
@@ -21,70 +29,25 @@
 	});
 
 	app.controller('HomeCtrl', function($scope, Page) {
-	    Page.setTitle("Welcome");
+	    Page.setTitle("Bienvenido");
+	});
+
+	app.controller('DemoCtrl', function($scope, Page) {
+	    Page.setTitle("AngularJS Demo");
+	    this.mensaje = "Hola Mundito!";
 	});
 
 	var app = angular.module('appServices', []);
 
-	app.factory('Page', function($rootScope){
-	            var pageTitle = "Untitled";
-	            return {
-	                title:function(){
-	                    return pageTitle;
-	                },
-	                setTitle:function(newTitle){
-	                    pageTitle = newTitle;
-	                }
-	            }
-	        });
-
-
-	/*
-	var app = angular.module('demoapp', [], function($routeProvider, $locationProvider) {
-	  $routeProvider.when('/inicio', {
-	    templateUrl: '/index.html',
-	    controller: HomeController
-	  });
-	  $routeProvider.otherwise( { redirectTo: '/inicio'} );
-
-	  // configure html5 to get links working
-	  // If you don't do this, you URLs will be base.com/#/home rather than base.com/home
-	  $locationProvider.html5Mode(true);
-	});*/
-
-	/*app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-		$stateProvider
-
-		.state('inicio', {
-	        url:'/',
-	        templateUrl: 'inicio.html'
-	        //controller: 'ControladorPrincipal'
-	    })
-
-		$urlRouterProvider.otherwise('/');
-	}]);*/
-
-	/*var cam = "a";
-    var test = "Hola Mundo!"
-
-	app.controller('ControladorJSON', ['$scope', '$http', function($scope, $http) {
-		//$scope.camaras = cam;
-		this.texto = cam;
-		var cams = this;
-
-		console.log("holiwi");
-		$http.get('json/canon_cameras.json').
-			success(function(data, status, headers, config) {
-			  // this callback will be called asynchronously
-			  // when the response is available
-			  console.log(status);
-			  cams = angular.fromJson(cams);
-			  
-			}).
-			error(function(data, status, headers, config) {
-			  // called asynchronously if an error occurs
-			  // or server returns response with an error status.
-			  console.log(status);
-			});
-	}]);*/
+	app.factory('Page', function($rootScope) {
+        var pageTitle = "Untitled";
+        return {
+            title:function(){
+                return pageTitle;
+            },
+            setTitle:function(newTitle){
+                pageTitle = newTitle;
+            }
+        }
+    });
 })();
